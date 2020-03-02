d4486847273be6368843851b5a8d08f63a104b73
Julian Humplstötter <julian@humplstoetter.com>
2020-03-02 15:30:21 +0100

    + initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * Aufgabe5.c
+ *
+ * Created: 3/2/2020 3:26:25 PM
+ * Author : Julian Humplstoetter
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	 DDRA = 0xff; // Setze Port F auf 
+	 PORTA = 0x01; // Schreibe 1 an Port F   
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
